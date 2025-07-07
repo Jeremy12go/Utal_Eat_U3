cb5f879990f003b1fbc8bbb7f834de94781b0dd7
Jeremy12go <jeremy12go@gmail.com>
2025-07-07 11:32:39 -0400

    integración de servicios a api gateway

---
+++ backend/service-gateway/gateway-app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+require('dotenv').config();
+
+const app = express();
+
+// Proxy para Accounts
+app.use('/accounts', createProxyMiddleware({
+  target: 'http://localhost:3000/accounts',
+  changeOrigin: true,
+}));
+
+// Proxy para Stores
+app.use('/stores', createProxyMiddleware({
+  target: 'http://localhost:3001/stores',
+  changeOrigin: true,
+}));
+
+// Proxy para Orders
+app.use('/orders', createProxyMiddleware({
+  target: 'http://localhost:3002/orders',
+  changeOrigin: true,
+}));
+
+// Proxy para Ratings
+app.use('/ratings', createProxyMiddleware({
+  target: 'http://localhost:3003/ratings',
+  changeOrigin: true,
+}));
+
+const PORT = process.env.PORT || 3004;
+app.listen(PORT, () => {
+  console.log(`API Gateway corriendo en http://localhost:${PORT}`);
+});
