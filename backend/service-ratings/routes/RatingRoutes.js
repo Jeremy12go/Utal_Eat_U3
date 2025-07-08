0209165f67b96b570ef8a3f98835727ffba67b86
Francomanx <143547506+Francomanx@users.noreply.github.com>
2025-07-08 16:06:33 -0400

    adicion de endpoint para recibir ratings de una tienda especifica (mal-hecho)
    En si esto si me llego a funcionar para enlazar los ratings a la tienda con id store1, pero la forma en la que se implemento fue la equivocada, y creo que algunas partes del codigo de la seccion añadida se pudieron haber trasladado al controller para que quedara mas ordenado... en todo caso si me da el tiempo puedo limpiarlo mas tarde, aunque no creo que vayamos a usar esta funcion nuevamente (espero)

--- backend/service-ratings/routes/RatingRoutes.js
+++ backend/service-ratings/routes/RatingRoutes.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/RatingController');
+const Rating = require('../models/Rating');
 
 router.get('/:idOrder', controller.getByIdOrder);
 router.get('/stores/:idStore', controller.getByIdStore);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.delete('/:id', controller.remove);
+
+//este es un endpoint que me va a devolver los ratings
+//de una tienda, y luego desde service-stores pedire estos
+//datos con axios
+router.get('/stores/:idStore', async (req, res) => {
+    try {
+        const ratings = await Rating.find({ idStore: req.params.storeId });
+        const ratingIds = ratings.map(r => r._id);
+        res.status(200).json(ratingIds);
+    } catch (err) {
+        res.status(500).json({ message: "Error al buscar ratings", error: err.message });
+    }
+});
+router.get('/store/:idStore/detalles', controller.getRatingsByStoreId);
+module.exports = router;
 
 module.exports = router;
