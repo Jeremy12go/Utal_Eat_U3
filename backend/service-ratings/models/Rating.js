b1c8efdf308d87edc3198f224a1174e25231777c
Francomanx <143547506+Francomanx@users.noreply.github.com>
2025-07-08 16:29:56 -0400

    adicion de variables en schema de rating
    Se añadieron unas variables que serian importantes para los ratings en general. entre estos, su propia id, ademas de la id del perfil asociado a dicho rating. numStars se cambio a stars y comment ya no es requerido en una calificacion puesto que los usuarios pueden decidir hacer calificaciones sin la necesidad de agregar un comentario

--- backend/service-ratings/models/Rating.js
+++ backend/service-ratings/models/Rating.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const ratingSchema = new mongoose.Schema({
+  id: { type: String, unique: true, required: true },
   idStore: { type: String, required: true},
   idOrder: { type: String, required: true },
-  numStars: { type: Number, required: true} ,
-  comment: { type: String, required: true}
-});
+  idProfile: { type: String, required: true },
+  stars: { type: Number, required: true, min:0, max:5 } ,
+  comment: { type: String }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Rating', ratingSchema);
